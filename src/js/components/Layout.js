7c7ba59f1f95950b5ae3002eb26c9fc99da4299b
Dvshah13 <dvshah13@gmail.com>
2017-02-18 12:49:25 -0600

    Practice with basic react login

---
+++ src/js/components/Layout.js
@@ -0,0 +1,50 @@
+import React from "react";
+import Header from "./Header";
+
+
+export default class Layout extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            search:"reactjs"
+        };
+        this.doSearch = this.doSearch.bind(this);
+    }
+
+    componentDidMount(){
+    }
+
+    doSearch(search,event){
+        console.log("doing the search:");
+        // console.log(a);
+        // console.log(b);
+        this.setState({
+            search:search
+        });
+
+        axios.get(`http://www.reddit.com/r/${search}.json`)
+          .then(res => {
+            const posts = res.data.data.children.map(obj => obj.data);
+            console.log(posts);
+          });
+    }
+
+    render() {
+        var displayShowResults = false;
+        let list = <li>Empty List</li>;
+
+        return (
+            <div>
+            <div id="SearchBar">
+                <SearchBar doSearch={this.doSearch} />
+            </div>
+            <div id="list">
+                <ResultList list={list} />
+            </div>
+            <Reddit subreddit={this.state.search}/>
+            </div>
+
+        );
+
+  }
+}
